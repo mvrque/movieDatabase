fc31f6efe21efebe6f34808b5e5de717578853f7
mvrque <marko_tomik@hotmail.com>
2022-06-15 22:59:15 +0300

    changed layout

--- script/js.js
+++ script/js.js
@@ -1,6 +1,6 @@
 
 document.querySelector('#searchMovie').addEventListener('click', searchByTitle)
-//document.querySelector('#topMovies').addEventListener('click', showTopMovies)
+
 
 
 document.querySelector('#movieName').addEventListener('keypress',function(event){
@@ -10,81 +10,37 @@ document.querySelector('#movieName').addEventListener('keypress',function(event)
 })
 
 function searchByTitle(){
-
-    
     const choice = document.querySelector('#movieName').value
-    const url = 'https://online-movie-database.p.rapidapi.com/auto-complete?q='+choice
+    const url = `https://movie-database-alternative.p.rapidapi.com/?s=${choice}&r=json&page=1`
     const options = {
         method: 'GET',
         headers: {
-            'X-RapidAPI-Host': 'online-movie-database.p.rapidapi.com',
-            'X-RapidAPI-Key': 'aafde9a81dmshcbc1fef151db9c0p1764ecjsnfc52d3dc0411'
+            'X-RapidAPI-Key': 'aafde9a81dmshcbc1fef151db9c0p1764ecjsnfc52d3dc0411',
+            'X-RapidAPI-Host': 'movie-database-alternative.p.rapidapi.com'
         }
     };
 
     fetch(url, options)
         .then(response => response.json())
         .then(response => { 
+            let data = response.Search
+            console.log(data)
+           
             
-            console.log(response)
-    
-                // for(el = 0 ; el <= response.d.length; el++){
-                //     console.log(response.d[2])
-                //     document.querySelector(`#name${el}`).innerText = response.d[el].l
-                //     document.querySelector(`#actors${el}`).innerText = response.d[el].s
-                //     document.querySelector(`#img${el}`).src = response.d[el].i.imageUrl
-                // }
-                let newArr = []
-                let el = 0;
-                while(el < response.d.length){
-                    if(response.d[el].q === 'feature'){
-                        newArr.push(response.d[el]) 
-                    }
-                    el++
-                }
-                console.log(newArr)
-                newArr.forEach(element => {
-                    let index = newArr.indexOf(element)
-                    console.log(index)
-                    document.querySelector(`#type${index}`).innerText = 'Movie'
-                    document.querySelector(`#name${index}`).innerText = response.d[index].l + '('+response.d[index].y+')'
-                    document.querySelector(`#actors${index}`).innerText = response.d[index].s
-                    document.querySelector(`#img${index}`).src = response.d[index].i.imageUrl
-                });
-                
+            const divList = document.getElementsByClassName('searchResult')
+            console.log(divList[0].childNodes[5].innerText)
+            
+            data.forEach((element, index) => {
+                //add movie image to the DOM
+                divList[index].childNodes[1].firstChild.src = element.Poster
+                //add movie name to the DOM
+                divList[index].childNodes[3].innerText = element.Title
+                //add movie year to the DOM
+                divList[index].childNodes[5].innerText = element.Year
+            });
         })
         .catch(err => console.error(err));
 }
 
 
 
-
-// function showTopMovies(){
-//     const options1 = {
-//         method: 'GET',
-//         headers: {
-//             'X-RapidAPI-Host': 'online-movie-database.p.rapidapi.com',
-//             'X-RapidAPI-Key': 'aafde9a81dmshcbc1fef151db9c0p1764ecjsnfc52d3dc0411'
-//         }
-//     };
-    
-//     fetch('https://online-movie-database.p.rapidapi.com/title/get-top-rated-movies', options1)
-//         .then(response => response.json())
-//         .then(response => console.log(response))
-//         .catch(err => console.error(err));
-
-
-        
-//     const options2 = {
-//         method: 'GET',
-//         headers: {
-//             'X-RapidAPI-Host': 'online-movie-database.p.rapidapi.com',
-//             'X-RapidAPI-Key': 'aafde9a81dmshcbc1fef151db9c0p1764ecjsnfc52d3dc0411'
-//         }
-//     };
-    
-//     fetch('https://online-movie-database.p.rapidapi.com/title/get-overview-details?tconst=tt0944947&currentCountry=US', options2)
-//         .then(data => data.json())
-//         .then(data => console.log())
-//         .catch(err => console.error(err));
-// }
